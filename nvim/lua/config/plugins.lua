dcb703db0502e425f37cd14dfc6d2e616b972b43
Dacheng Gao <successgdc@gmail.com>
2026-06-13 21:44:38 +0800

    feat(nvim): add vscode colorscheme and indent guides

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -1,11 +1,17 @@
 -- Plugins are managed by the native vim.pack (nvim 0.12+).
 -- Update with :lua vim.pack.update()
 vim.pack.add({
+  "https://github.com/Mofiqul/vscode.nvim",
+  "https://github.com/lukas-reineke/indent-blankline.nvim",
   "https://github.com/stevearc/oil.nvim",
   "https://github.com/nvim-mini/mini.pick",
   -- master branch is frozen; main is the maintained rewrite
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
+
+-- colorscheme; follows vim.o.background (set in config.options)
+require("vscode").setup()
+require("vscode").load()
 
 require("oil").setup({
   view_options = {
@@ -16,6 +22,12 @@ require("oil").setup({
 })
 
 require("mini.pick").setup()
+
+-- indentation guides; scope highlight uses treesitter
+require("ibl").setup({
+  indent = { char = "▏" },
+  scope = { char = "▏", enabled = true, show_start = false, show_end = false },
+})
 
 -- no-op for already installed parsers
 require("nvim-treesitter").install({
